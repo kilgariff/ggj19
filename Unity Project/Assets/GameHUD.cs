91306e89f082479f845bdacc6a63e1ea33517752
Jim Kennedy <Computron59@gmail.com>
2019-01-26 15:04:53 +0000

    Auto stash before merge of "master" and "origin/master"

---
+++ Unity Project/Assets/GameHUD.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class GameHUD : MonoBehaviour
+{
+    public Text timerText;
+    public float timeLeft = 900;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timerText.text = "IKEA closing:  " + (Mathf.Floor(timeLeft)).ToString();
+    }
+}
