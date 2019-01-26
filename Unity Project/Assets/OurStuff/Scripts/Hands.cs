caae74c0016b9bf7a18209f39a050b20e621f114
Ross Kilgariff <rosskilgariff@googlemail.com>
2019-01-26 15:03:50 +0000

    Add basic walls and ceiling

---
+++ Unity Project/Assets/OurStuff/Scripts/Hands.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hands : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float right_trigger = Input.GetAxis("AXIS_10");
+        Debug.Log("Right hand trigger axis: " + right_trigger);
+    }
+}
