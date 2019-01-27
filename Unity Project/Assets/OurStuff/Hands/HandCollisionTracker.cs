7c1a71bd64331a2f69524bdcc79108d2d9a8acfe
Ross Kilgariff <rosskilgariff@googlemail.com>
2019-01-27 16:05:08 +0000

    Audio and watch stuff

---
+++ Unity Project/Assets/OurStuff/Hands/HandCollisionTracker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCollisionTracker : MonoBehaviour
+{
+    public GameObject hovering = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hovering = other.gameObject;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (hovering == other.gameObject)
+        {
+            hovering = null;
+        }
+    }
+}
